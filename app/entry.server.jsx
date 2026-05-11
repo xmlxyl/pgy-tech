7d02f87fff3afb532d2ec08ddc2e770217b0eb8c
ylXian <xianyili812@163.com>
2026-05-11 09:37:47 +0800

    add remix dev dependency

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -1,51 +1,21 @@
-import { PassThrough } from "stream";
-import { renderToPipeableStream } from "react-dom/server";
-import { ServerRouter } from "react-router";
-import { createReadableStreamFromReadable } from "@react-router/node";
-import { isbot } from "isbot";
+import { handleRequest as vercelHandleRequest } from "@vercel/react-router/entry.server";
 import { addDocumentResponseHeaders } from "./shopify.server";
 
-export const streamTimeout = 5000;
+export { streamTimeout } from "@vercel/react-router/entry.server";
 
 export default async function handleRequest(
   request,
   responseStatusCode,
   responseHeaders,
   reactRouterContext,
+  loadContext,
 ) {
   addDocumentResponseHeaders(request, responseHeaders);
-  const userAgent = request.headers.get("user-agent");
-  const callbackName = isbot(userAgent ?? "") ? "onAllReady" : "onShellReady";
-
-  return new Promise((resolve, reject) => {
-    const { pipe, abort } = renderToPipeableStream(
-      <ServerRouter context={reactRouterContext} url={request.url} />,
-      {
-        [callbackName]: () => {
-          const body = new PassThrough();
-          const stream = createReadableStreamFromReadable(body);
-
-          responseHeaders.set("Content-Type", "text/html");
-          resolve(
-            new Response(stream, {
-              headers: responseHeaders,
-              status: responseStatusCode,
-            }),
-          );
-          pipe(body);
-        },
-        onShellError(error) {
-          reject(error);
-        },
-        onError(error) {
-          responseStatusCode = 500;
-          console.error(error);
-        },
-      },
-    );
-
-    // Automatically timeout the React renderer after 6 seconds, which ensures
-    // React has enough time to flush down the rejected boundary contents
-    setTimeout(abort, streamTimeout + 1000);
-  });
+  return vercelHandleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    reactRouterContext,
+    loadContext,
+  );
 }
